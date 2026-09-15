6461b7be7ce8846ca45599dd1797739a54d90f87
Victor <victorbelchiord@gmail.com>
2026-09-15 19:12:37 +0100

    mudanças do vite

--- hello-vite/src/components/Header.jsx
+++ hello-vite/src/components/Header.jsx
@@ -1,6 +1,9 @@
+import headerLogo from "../assets/logo.png";
+
 function Header() {
   return (
     <div>
+      <img src={headerLogo} alt="TripleTen logo" />
       <h1>Olá, Vite!</h1>
     </div>
   );
